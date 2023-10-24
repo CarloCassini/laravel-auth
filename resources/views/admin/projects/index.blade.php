6a7903df9f5bd704ee711c4dfae907e3bd7b054f
CarloCassini <c.cassini87@gmail.com>
2023-10-24 15:13:28 +0200

    better index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,10 +1,38 @@
 @extends('layouts.app')
-
+ 
+@section('import-cdn')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+ 
 @section('content')
 
-<div class="contatiner mt-5">
+<div class="container mt-5">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        
+        @foreach ($projects as $project)
+        <tbody>
+          <tr>
+            <th scope="row">{{$project->id}}</th>
+            <td>{{$project->name}}</td>
+            <td>{{$project->slug}}</td>
+            <td>{{$project->git_url}}</td>
+            <td>{{$project->description}}</td>
+            <td><i class="fa-solid fa-eye"></i></td>
+            <td><i class="fa-solid fa-file-pen"></i></td>
+            <td><i class="fa-solid fa-trash"></i></td>
 
-    @dump($projects)
+          </tr>
+        </tbody>
+        @endforeach
+    </table>
 </div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    {{-- importo le cdn --}}
+    @yield('import-cdn')
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
   </head>
